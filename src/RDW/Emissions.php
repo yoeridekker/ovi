8391673f10daa9a35de8b2cab7dd027fbfad41a3
Yoeri Dekker <yoeri@3eighty.nl>
2019-10-24 16:11:54 +0200

    Added new file to get emissions

---
+++ src/RDW/Emissions.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ovi\RDW;
+
+use Ovi\Interfaces\ApiInterface;
+use Ovi\Traits\SanitizationTrait;
+use Ovi\Traits\ValidationTrait;
+use Ovi\Traits\ApiTrait;
+
+class Emissions implements ApiInterface
+{
+
+    use SanitizationTrait;
+    use ValidationTrait;
+    use ApiTrait;
+
+    private $api_base = 'https://opendata.rdw.nl';
+    private $api_path = 'resource/5w6t-p66a.json';
+    
+    private $allowed_params = array(
+        'eu_type_goedkeuringssleutel' => [
+            'required' => true,
+        ],
+    );
+
+    protected $fields = [];
+    public $request_url = '';
+    public $query_vars = [];
+
+    public function enrichData() : object 
+    {
+        return $this;
+    }
+
+}
